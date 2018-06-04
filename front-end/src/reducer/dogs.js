13d557466fdfa0dfb8401f6de253f7da421de565
Josiah Green <JosiahGreen90@gmail.com>
2018-06-04 14:50:44 -0700

    wrote validator and combined reducers

---
+++ front-end/src/reducer/dogs.js
@@ -0,0 +1,21 @@
+import { validateDog } from '../utils';
+
+const emptyState = [];
+
+export default (state = emptyState, { type, payload }) => {
+  switch (type) {
+    case 'DOGS_FETCH':
+      return payload;
+    case 'DOG_CREATE':
+      validateDog(payload);
+      return [payload, ...state];
+    case 'DOG_UPDATE':
+      validateDog(payload);
+      return state.map(item => (item._id === payload._id ? payload : item));
+    case 'DOG_DELETE':
+      validateDog(payload);
+      return state.filter(item => item._id !== payload._id);
+    default: 
+      return state;
+  }
+}
