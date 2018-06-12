bbc9914a864dc2193cb015ab8e9e381eeabad370
Josiah Green <JosiahGreen90@gmail.com>
2018-06-12 10:17:40 -0700

    working on auth action

--- front-end/src/actions/client-profile.js
+++ front-end/src/actions/client-profile.js
@@ -1,25 +1,21 @@
 import superagent from 'superagent';
 import * as routes from '../routes';
 
-// TODO: Linter error here
 
-// SYNC
 const setProfile = profile => ({
   type: 'CLIENT_PROFILE_SET',
-  payload: 'profile',
+  payload: profile,
 });
 
-// ASYNC
 const createRequest = profile => (store) => {
   const { token } = store.getState();
 
   return superagent.post(`${API_URL}${routes.PROFILE_ROUTE}`)
-    .set('Authorization', `Bearer ${token}`) // HTTP header, string
+    .set('Authorization', `Bearer ${token}`)
     .set('Content-Type', 'application/json')
     .send(profile)
     .then((response) => {
       return store.dispatch(setProfile(response.body));
-      // TODO: check the body.value it could be just body or something else
     });
 };
 
@@ -35,10 +31,10 @@ const updateRequest = profile => (store) => {
     });
 };
 
-const fetchRequest = profile => (store) => {
+const fetchRequest = () => (store) => {
   const { token } = store.getState();
 
-  return superagent.get(`${API_URL}${routes.PROFILE_ROUTE}/${profile._id}`)
+  return superagent.get(`${API_URL}${routes.PROFILE_ROUTE}/me`)
   // TODO: check the route for this. Sluggram was /me not/id
     .set('Authorization', `Bearer ${token}`)
     .then((response) => {
